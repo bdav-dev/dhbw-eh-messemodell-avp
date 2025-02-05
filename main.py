a83f81bdb3ef715c4e470c817d00107061f786b9
bdav-dev <david.berezowski1@gmail.com>
2025-02-05 16:04:49 +0100

    Pump control implementation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ DHBW Lörrach - E+H Messtechnik-Messemodell
 Projektrealisierung WWI21A | Lounis Ammour, Marvin Kern | März 2024
 
 """
-from machine import Pin, ADC, SoftI2C
+from machine import Pin, ADC, SoftI2C, PWM
 import ssd1306
 import time
 import network
@@ -13,27 +13,72 @@ from umqtt.simple import MQTTClient
 
 WLAN_SSID = "DHBW-Loe-EH-Messemodell"
 WLAN_PASSWORD = "dhbw2024"
+
 MQTT_CLIENT_ID = "client"
 MQTT_BROKER = "192.168.220.1"
 MQTT_USER = "dhbw-loe-eh-messemodell"
 MQTT_PASSWORD = "dhbw2024"
 MQTT_PUB_TOPIC = "data"
-MQTT_PUB_TOPIC_INPUT = "input_mode_esp"
-MQTT_SUB_TOPIC = "input_mode"
+MQTT_PUB_TOPIC_FLOWRATE_INPUT_MODE_SET_INITIAL = "flowrate_input_mode_set_initial"
+MQTT_SUB_TOPIC_FLOWRATE_INPUT_MODE = "flowrate_input_mode"
+MQTT_SUB_TOPIC_PUMPCTRL = "pumpctrl"
+
 OLED_TITLE = "DHBW Messemodell"
 
-demo_mode = False
-pot_rma42 = ADC(Pin(34))
+# Enums are not supported in MicroPython
+class Flowrate_Input:
+  TWO_PIN_INPUT = { "value": 0, "name": "2-PIN" }
+  POTENTIOMETER = { "value": 1, "name": "POTI" }
+  PUMPSETTING = { "value": 2, "name": "PUMP" }
+
+flowrate_input_mode = Flowrate_Input.TWO_PIN_INPUT
+
+pot_rma42 = ADC(Pin(34))   # POTI 32
 pot_poti = ADC(Pin(32))
 # Full range: 3.3v
-pot_rma42.atten(ADC.ATTN_11DB)    
+pot_rma42.atten(ADC.ATTN_11DB)
 pot_poti.atten(ADC.ATTN_11DB)
 
-# OLED Display
+pump_pin = PWM(Pin(26))
+pump_pin.freq(200)
+pump_pin.duty(0)
+
+flowrate_pumpsetting_simulation_value = 0
+
+# OLED Display Initialisierung
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))   # ESP32 Pin assignment
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-# Update OLED Helper-Function
+def parse_mqtt_sub_flowrate_input_mode_value(value):
+  try:
+    int_value = int(value)
+  except:
+    return Flowrate_Input.TWO_PIN_INPUT
+
+  if int_value == 1:
+    return Flowrate_Input.POTENTIOMETER
+  elif int_value == 2:
+    return Flowrate_Input.PUMPSETTING
+
+  return Flowrate_Input.TWO_PIN_INPUT
+
+def set_pumplevel(value):
+  global flowrate_pumpsetting_simulation_value
+
+  try:
+    int_value = int(value)
+  except:
+    return
+
+  if int_value < 0:
+    int_value = 0
+  elif int_value > 100:
+    int_value = 100
+
+  pump_pin.duty(int(1023 * (int_value / 100)))
+  flowrate_pumpsetting_simulation_value = int_value / 100 * 4095
+
+# OLED aktualisieren
 def update_oled(start, *lines):
     oled.fill(0)
     oled.text(f'{OLED_TITLE}', 0, 0)
@@ -60,26 +105,30 @@ def connect_wifi():
   update_oled(3, 'WLAN', 'Connected!')
   time.sleep(2)
 
-# MQTT Connection
+# MQTT Startup
 def connect_mqtt():
   print("MQTT | Connecting to MQTT broker... ")
   update_oled(3, 'MQTT', 'Connecting...')
   client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_PASSWORD)
   client.set_callback(sub_cb)
   client.connect()
-  client.subscribe(MQTT_SUB_TOPIC)
-  print(f'MQTT | Connected to {MQTT_BROKER} MQTT broker, subscribed to {MQTT_SUB_TOPIC} topic')
+  client.subscribe(MQTT_SUB_TOPIC_FLOWRATE_INPUT_MODE)
+  client.subscribe(MQTT_SUB_TOPIC_PUMPCTRL)
+  print(f'MQTT | Connected to {MQTT_BROKER} MQTT broker, subscribed to {MQTT_SUB_TOPIC_FLOWRATE_INPUT_MODE} and {MQTT_SUB_TOPIC_PUMPCTRL} topic')
   update_oled(3, 'MQTT', 'Connected!')
   time.sleep(2)
   return client
 
-# Callback for MQTT: Subscribe to input_mode topic
+# Callback für MQTT: Subscribe to input_mode topic
 def sub_cb(topic, msg):
-  global demo_mode
+  global flowrate_input_mode
+  global pump_level
+  
   print(f'MQTT | Incoming message from {topic}: {msg}')
-  if topic == b'input_mode':
-    if msg == b'true': demo_mode = True
-    elif msg == b'false': demo_mode = False
+  if topic == MQTT_SUB_TOPIC_FLOWRATE_INPUT_MODE.encode("UTF-8"):
+    flowrate_input_mode = parse_mqtt_sub_flowrate_input_mode_value(msg)
+  elif topic == MQTT_SUB_TOPIC_PUMPCTRL.encode("UTF-8"):
+    set_pumplevel(msg)
 
 # Restart ESP
 def restart():
@@ -93,6 +142,7 @@ def restart():
 def calc_flowrate(adc_value):
   flow_rate = 0 if float(adc_value) < 0.5 else round((9.5 / 4095.0 * float(adc_value) + 0.5), 2)
   return "{:.2f}".format(flow_rate)
+  #return 0 if float(adc_value) < 0.5 else round((9.5 / 4095.0 * float(adc_value) + 0.5), 2)
 
 def main():
   print("OS | Starting...")
@@ -109,7 +159,7 @@ def main():
   # Connect to MQTT-Server
   try:
     client = connect_mqtt()
-    client.publish(MQTT_PUB_TOPIC_INPUT, f"{demo_mode}")
+    client.publish(MQTT_PUB_TOPIC_FLOWRATE_INPUT_MODE_SET_INITIAL, str(flowrate_input_mode["value"]))
   except OSError as e:
     print('MQTT | Failed to connect to MQTT broker. Restarting...')
     update_oled(3, 'MQTT', 'Failed to', 'connect!')
@@ -117,22 +167,29 @@ def main():
     restart()
 
   while True:
-    try:           
-      flowrate = calc_flowrate((pot_poti.read() if demo_mode else pot_rma42.read()))
-      update_oled(1, f'Input: {('POTI' if demo_mode else 'EH-RMA42')}', '', '', 'Flowrate:', f'{flowrate} m/s')
+    try:
+      raw_flowrate = 0
+      if flowrate_input_mode == Flowrate_Input.TWO_PIN_INPUT:
+         raw_flowrate = pot_rma42.read()
+      elif flowrate_input_mode == Flowrate_Input.POTENTIOMETER:
+         raw_flowrate = pot_poti.read()
+      elif flowrate_input_mode == Flowrate_Input.PUMPSETTING:
+         raw_flowrate = flowrate_pumpsetting_simulation_value
 
+      flowrate = calc_flowrate(raw_flowrate)
+      
+      update_oled(1, f'Input: {flowrate_input_mode["name"]}', '', '', 'Flowrate:', f'{flowrate} m/s')
       message = ujson.dumps({"flowrate": flowrate})
-      print("MQTT | Reporting to MQTT topic {}: {}".format(MQTT_PUB_TOPIC, message))
-
+      print(f"MQTT | {flowrate_input_mode["name"]} | Publishing flowrate: {flowrate}")
+      
       if not network.WLAN(network.STA_IF).isconnected():
         print('Network | Connection lost. Restarting...')
         update_oled(3, 'WLAN', 'Connection lost!')
         time.sleep(5)
         restart()
-      
+
       client.publish(MQTT_PUB_TOPIC, message)
       client.check_msg()
-      
       time.sleep(0.1)
     except OSError as e:
       restart()
